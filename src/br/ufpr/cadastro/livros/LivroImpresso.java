d966f59ac4ce75b0617b39847ac1afbdcb8eec7a
Lucas Sônego <sonego_lucas@outlook.com>
2019-09-23 14:29:45 -0300

    Criada classe LivroImpresso

---
+++ src/br/ufpr/cadastro/livros/LivroImpresso.java
@@ -0,0 +1,42 @@
+package br.ufpr.cadastro.livros;
+
+public class LivroImpresso extends Livros {
+    private String capa;
+    private String peso;
+    private String dimensoes;
+
+    @Override
+    public void mostrarInformacoes() {
+        System.out.println("\n\n=-=-=-=-=- Livro -=-=-=-=-=");
+        super.mostrarInformacoes();
+        System.out.println("Tipo de capa: " + this.getCapa());
+        System.out.println("Dimensoes: " + this.getDimensoes());
+        System.out.println("Peso: " + this.getPeso());
+        this.autor.mostrarInformacoes();
+    }
+
+    public String getCapa() {
+        return this.capa;
+    }
+
+    public void setCapa(String capa) {
+        this.capa = capa;
+    }
+
+    public String getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getDimensoes() {
+        return this.dimensoes;
+    }
+
+    public void setDimensoes(String dimensoes) {
+        this.dimensoes = dimensoes;
+    }
+    
+}
